3656a520adc03438fba6d361372cb484ee4aa318
ManojAnnam <annam.manoj1996@gmail.com>
2019-06-29 00:22:10 +0530

    Added AutoMapperProfile Class

---
+++ EmployeeManagementAPI/EmployeeManagementAPI.Business/AutoMapperProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using EmployeeManagementAPI.Models.Models;
+using EmployeeManagementAPI.Repository.Entities;
+
+
+namespace EmployeeManagementAPI.Business
+{
+    class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<EmployeeDetails, EmployeeDetailsModel>();
+        }
+    }
+}
